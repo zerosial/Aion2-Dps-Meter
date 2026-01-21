1cd63e98940fa79b58c171a66b27fcdcded76893
TK-open-public <itaebread2@gmail.com>
2026-01-21 23:13:58 +0900

    Fix: 전투 시작-끝 타임 측정 방식 변경

--- src/main/kotlin/entity/TargetInfo.kt
+++ src/main/kotlin/entity/TargetInfo.kt
@@ -5,7 +5,7 @@ import java.util.UUID
 data class TargetInfo(
     private val targetId: Int,
     private var damagedAmount: Int = 0,
-    private val targetDamageStarted: Long,
+    private var targetDamageStarted: Long,
     private var targetDamageEnded: Long,
     private val processedUuid: MutableSet<UUID> = mutableSetOf(),
 ) {
@@ -24,6 +24,12 @@ data class TargetInfo(
     fun processPdp(pdp:ParsedDamagePacket){
         if (processedUuid.contains(pdp.getUuid())) return
         damagedAmount += pdp.getDamage()
+        val ts = pdp.getTimeStamp()
+        if (ts < targetDamageStarted){
+            targetDamageStarted = ts
+        } else if (ts > targetDamageEnded){
+            targetDamageEnded = ts
+        }
         targetDamageEnded = pdp.getTimeStamp()
         processedUuid.add(pdp.getUuid())
     }
