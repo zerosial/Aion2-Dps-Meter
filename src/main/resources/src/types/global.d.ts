89b82db4e8a1854883f1bff132308c811609a31d
Owl33 <wianw@naver.com>
2026-03-21 13:40:43 +0900

    2026-03-21 누락 CSS 추가

--- src/main/resources/src/types/global.d.ts
+++ src/main/resources/src/types/global.d.ts
@@ -5,8 +5,7 @@ declare global {
     javaBridge?: {
       resetDps?: () => void;
       moveWindow?: (x: number, y: number) => void;
-      startKeyCapture: () => void;
-      stopKeyCapture: () => void;
+
       getDpsData?: () => void;
       getBattleDetail?: (id: number) => Promise<any>;
       getVersion?: () => void;
